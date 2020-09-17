725d303f8e6a987ae06926f1e9f0c5f0e3b378bd
Andrew <andrew@banausia.com>
2020-09-17 18:03:05 +0200

    Inaugurate.

---
+++ README.md
@@ -0,0 +1,31 @@
+# Resolarized
+
+*Resolarized* is a minimal Solarized theme for the Textual IRC client.
+
+It features:
+  - a smooth, unobtrusive Solarized palette
+  - an adaptive light/dark color scheme based on Textual's appearance (and transitively, system appearance)
+  - responsive layout and type, designed to work across common window sizes and widths (full screen, ½ screen, ⅓ screen)
+
+Resolarized is currently LTR-only, with apologies.
+
+
+## Installation
+
+Clone this repository or copy its contents into the Textual user styles folder:
+
+```bash
+# for the Mac App Store version
+git clone https://github.com/tapeinosyne/resolarized-textual "$HOME/Library/Group Containers/8482Q6EPL6.com.codeux.irc.textual/Library/Application Support/Textual/Styles/"
+
+# for the standalone version
+git clone https://github.com/tapeinosyne/resolarized-textual "$HOME/Library/Group Containers/com.codeux.apps.textual/Library/Application Support/Textual/Styles/"
+…
+```
+
+You can open a Finder window directly therein by clicking this `textual://` link to the [textual://custom-styles-folder](custom styles folder).
+
+
+## Configuration
+
+You might want to consider ticking `Disable nickname colors` in the `Style / Advanced` menu of the Textual preferences, as themes cannot trivially set custom colors and the defaults can be rather garish.
---
+++ scripts.js
@@ -0,0 +1,127 @@
+/*
+Resolarized
+*/
+
+'use strict';
+
+const Resolarized = {
+	consecutiveClass: "consecutive",
+
+	getPreviousLine(line) {
+		let previousLine = line.previousElementSibling;
+		if (previousLine && previousLine.id === "mark") {
+			previousLine = previousLine.previousElementSibling;
+		}
+
+		if (previousLine &&
+			previousLine.classList &&
+			previousLine.classList.contains("line")
+		) { return previousLine; }
+		else { return null; }
+	},
+
+	getSenderElement(line) {
+		return line ? line.querySelector(".sender") : null;
+	},
+
+	getSenderNickname(line) {
+		const sender = Resolarized.getSenderElement(line);
+		return sender ? sender.dataset.nickname : null;
+	},
+
+	handleConsecutiveMessages(current, previous) {
+		const previousSender = Resolarized.getSenderNickname(previous);
+		const currentSender = Resolarized.getSenderNickname(current);
+
+		if (currentSender === null || previousSender === null) {
+			return;
+		}
+
+		const consecutive = currentSender === previousSender;
+		const firstConsecutive = consecutive && !previous.classList.contains(Resolarized.consecutiveClass);
+
+		if (consecutive) {
+			const anchorId = firstConsecutive ? previous.id : previous.dataset.resolarizedAnchor;
+
+			current.dataset.resolarizedAnchor = anchorId;
+			current.classList.add(Resolarized.consecutiveClass);
+
+			Resolarized.linkHoverToSender(current);
+		}
+	},
+
+	linkHoverToSender(line) {
+		const remoteHoverClass = "hover-on-sibling";
+
+		line.addEventListener("mouseover", () => {
+			const visibleSenderId = line.dataset.resolarizedAnchor;
+			let senderDisplay = document.getElementById(visibleSenderId);
+			if (senderDisplay) {
+				senderDisplay.classList.add(remoteHoverClass);
+			}
+		}, { passive: true });
+
+		line.addEventListener("mouseleave", () => {
+			const visibleSenderId = line.dataset.resolarizedAnchor;
+			let senderDisplay = document.getElementById(visibleSenderId);
+			if (senderDisplay) {
+				senderDisplay.classList.remove(remoteHoverClass);
+			}
+		}, { passive: true });
+	},
+
+	setLightness(lightness) {
+		const body = document.body;
+		switch (lightness) {
+			case "light": {
+				body.dataset.themeLightness = "light";
+				break;
+			}
+			case "dark": {
+				body.dataset.themeLightness = "dark";
+				break;
+			}
+			default: delete body.dataset.themeLightness;
+		}
+	},
+};
+
+/*
+Textual API calls
+*/
+
+/* ESLint Configuration */
+/* global app, Textual, ConversationTracking */
+/* Defined in: "Textual.app -> Contents -> Resources -> JavaScript -> API -> core.js" */
+
+/* WONTFIX (not worth the effort): The `lightness` setting comes into
+effect too late: there is always a flash of the default color scheme
+(dark) before the view switches over to the appropriate one. Moreover,
+there is no color or opacity transition.
+
+This solely affects full theme reload, which only happens when users
+change style settings or during development.
+*/
+Textual.viewBodyDidLoad = () => {
+	app.appearance(lightness => {
+		Resolarized.setLightness(lightness);
+	});
+
+	Textual.fadeOutLoadingScreen(1.0, 1.0);
+};
+
+Textual.appearanceDidChange =
+	(lightness) => Resolarized.setLightness(lightness);
+
+Textual.nicknameSingleClicked =
+	(e) => ConversationTracking.nicknameSingleClickEventCallback(e);
+
+Textual.messageAddedToView = (lineNumber) => {
+	const currentLine = document.getElementById("line-" + lineNumber);
+	const previousLine = Resolarized.getPreviousLine(currentLine);
+	if (currentLine && previousLine) {
+		Resolarized.handleConsecutiveMessages(currentLine, previousLine);
+	}
+
+	ConversationTracking.updateNicknameWithNewMessage(currentLine);
+};
